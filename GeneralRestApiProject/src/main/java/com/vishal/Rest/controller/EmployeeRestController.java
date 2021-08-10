9d722bae333083a4f7999b5a9889d0867fb1f357
vishal <vishalambekar459@gmail.com>
2021-08-10 18:43:02 +0530

    getEmployeeAdded

--- GeneralRestApiProject/src/main/java/com/vishal/Rest/controller/EmployeeRestController.java
+++ GeneralRestApiProject/src/main/java/com/vishal/Rest/controller/EmployeeRestController.java
@@ -1,6 +1,11 @@
 package com.vishal.Rest.controller;
 
+
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +15,39 @@ import com.vishal.Rest.Entities.Employee;
 import com.vishal.Rest.Repositories.EmployeeRepository;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employee") 
 public class EmployeeRestController {
 
+	//https://spring.io/guides/tutorials/rest/     refer this page to do further
+	
 	@Autowired
 	EmployeeRepository employeeRepo;
 	
-	@GetMapping("/{empId}")
-	public Employee getEmployee(@PathVariable int empId)
+	@GetMapping(path="/{empId}")
+	public Employee getEmployee(@PathVariable int empId) throws Exception
 	{
-		Employee getEmployeejson=employeeRepo.getById(empId);
 		
-		System.out.println("getEmployeejson :: "+getEmployeejson);
+		Employee emp= employeeRepo.findById(empId).orElse(new Employee());
 		
-		return getEmployeejson;
+		return emp;
 	}
+	
+
 }
+
+
+
+
+
+
+
+
+
+
+//@GetMapping(path="/{empId}")
+//public Employee getEmployee(@PathVariable int empId) throws Exception
+//{
+//	
+//	return employeeRepo.findById(empId)
+//		      .orElseThrow(() -> new Exception("no employee foind"));
+//}
